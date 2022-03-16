1dbb19894bdfe79db3ef5bb45b6e614f5ca2f202
Benite Ribeiro <beniterm@gmail.com>
2022-03-16 08:58:38 -0300

    Estrutura pronta

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nodets-canil
+
+Projeto feito no módulo do curso Node + Typescript
+
+### Pré-requisitos globais:
+
+`npm i -g nodemon typescript ts-node`
+
+### Instação
+
+`npm install`
+
+### Rodar o projeto
+
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import mustache from "mustache-express";
+import path from "path";
+import mainRoutes from "./routes/index";
+
+dotenv.config();
+
+const server = express();
+
+// configurar o template engine
+server.set("view engine", "mustache");
+server.set("views", path.join(__dirname, "views"));
+server.engine("mustache", mustache());
+
+// configurar a pasta pública
+server.use(express.static(path.join(__dirname, "../public")));
+
+// rotas
+server.use(mainRoutes);
+
+// página não encontrada
+server.use((req, res) => {
+	res.send("Página não encontada");
+});
+
+// rodar o servidor (a linha abaixo deve ser a última deste módulo)
+server.listen(process.env.PORT);
